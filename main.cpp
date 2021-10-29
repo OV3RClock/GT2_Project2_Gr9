a28bdaa67d2781d63af1b299ac88a2081d17a53b
OV3RClock <40008329+OV3RClock@users.noreply.github.com>
2021-10-29 17:43:33 +0200

    Déplacements ok

--- main.cpp
+++ main.cpp
@@ -4,25 +4,43 @@ using namespace sf;
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::RenderWindow window(sf::VideoMode(1280, 720), "SFML works!");
+    sf::RectangleShape entity(sf::Vector2f(100.f, 100.f));
+
     Texture texture;
     texture.loadFromFile("ghost.png");
+
     Sprite sprite;
-    sprite.setTexture(texture);
+    entity.setTexture(&texture);
 
     while (window.isOpen())
     {
         sf::Event event;
         while (window.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed)
-                window.close();
+            if (event.type == sf::Event::Closed){window.close();}  
+            if (event.type == sf::Event::KeyPressed)
+            {
+                switch (event.key.code)
+                {
+                    case Keyboard::Z:
+                        entity.move(sf::Vector2f(0.f, -20.f));
+                        break;
+                    case Keyboard::Q:
+                        entity.move(sf::Vector2f(-20.f, 0.f));
+                        break;
+                    case Keyboard::S:
+                        entity.move(sf::Vector2f(0.f, 20.f));
+                        break;
+                    case Keyboard::D:
+                        entity.move(sf::Vector2f(20.f, 0.f));
+                        break;
+                }
+            }
         }
 
         window.clear();
-        window.draw(shape);
+        window.draw(entity);
         window.draw(sprite);
         window.display();
     }
