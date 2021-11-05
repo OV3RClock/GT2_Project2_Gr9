d39e79925c2acb29c701be6147af99d91c1e6be3
OVERClock <40008329+valentincornet@users.noreply.github.com>
2021-11-05 22:53:08 +0100

    Enfin ! (après 3h de recherche)
    Déplacement pi-xel c:

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@
 using namespace sf;
 using namespace std;
 
-void normalize(Vector2f& velocity, float speed) {
+void normalize(Vector2f &velocity, float speed) {
     float norme = sqrt((velocity.x * velocity.x) + (velocity.y * velocity.y));
     if (norme != 0)
     {
@@ -21,9 +21,10 @@ int main()
 {
     int dim = 16; // Ne pas changer (taille des tiles de foresttiles2-t.png)
     int scale = 4;
+
     #pragma region INIT
 
-        sf::RenderWindow window(sf::VideoMode((dim*scale*12), (dim*scale*8)), "The game seems to be working...");
+        sf::RenderWindow window(sf::VideoMode((dim*scale*12), (dim*scale*8)), "The game seems to be working..."); // La map possede 12 colones et 8 lignes
         window.setKeyRepeatEnabled(false);
 
         #pragma region Player
@@ -32,12 +33,8 @@ int main()
             texture.loadFromFile("ghost.png");
             Sprite sprite;
             entity.setTexture(&texture);
-            float speed = 0.5;
             sf::Vector2f velocity;
-            bool z = false;
-            bool q = false;
-            bool s = false;
-            bool d = false;
+            float speed = 0.5;
         #pragma endregion
 
         #pragma region Map
@@ -53,73 +50,37 @@ int main()
     while (window.isOpen())
     {
         Event event;
-        
         while (window.pollEvent(event))
         {
             if (event.type == Event::Closed) { window.close(); }
-            /*
-            if (Keyboard::isKeyPressed(Keyboard::Z)) { velocity.y -= speed; }
-            if (Keyboard::isKeyPressed(Keyboard::Q)) { velocity.x -= speed; }
-            if (Keyboard::isKeyPressed(Keyboard::S)) { velocity.y += speed; }
-            if (Keyboard::isKeyPressed(Keyboard::D)) { velocity.x += speed; }
-            
-            if (!Keyboard::isKeyPressed(Keyboard::Z)) { velocity.y += abs(velocity.y); }
-            if (!Keyboard::isKeyPressed(Keyboard::Q)) { velocity.x += abs(velocity.x); }
-            if (!Keyboard::isKeyPressed(Keyboard::S)) { velocity.y -= velocity.y; }
-            if (!Keyboard::isKeyPressed(Keyboard::D)) { velocity.x -= velocity.x; }
-            */
-            
-            if (event.type == Event::KeyPressed)
-            {
-                switch (event.key.code)
-                {
-                    case Keyboard::Z:
-                        z = true;
-                        break;
-                    case Keyboard::Q:
-                        q = true;
-                        break;
-                    case Keyboard::S:
-                        s = true;
-                        break;
-                    case Keyboard::D:
-                        d = true;
-                        break;
-                }
-            }
             if (event.type == Event::KeyReleased)
             {
                 switch (event.key.code)
                 {
                     case Keyboard::Z:
-                        z = false;
+                        velocity.y -= velocity.y;
                         break;
                     case Keyboard::Q:
-                        q = false;
+                        velocity.x -= velocity.x;
                         break;
                     case Keyboard::S:
-                        s = false;
+                        velocity.y -= velocity.y;
                         break;
                     case Keyboard::D:
-                        d = false;
+                        velocity.x -= velocity.x;
                         break;
                 }
             }
-
-            if (z) { velocity.y -= speed; }
-            if (q) { velocity.x -= speed; }
-            if (s) { velocity.y += speed; }
-            if (d) { velocity.x += speed; }
-
-            if (!z) { velocity.y = 0; }
-            if (!q) { velocity.x = 0; }
-            if (!s) { velocity.y = 0; }
-            if (!d) { velocity.x = 0; }
         }
+
+        if (Keyboard::isKeyPressed(Keyboard::Z)) { velocity.y -= speed; }
+        if (Keyboard::isKeyPressed(Keyboard::Q)) { velocity.x -= speed; }
+        if (Keyboard::isKeyPressed(Keyboard::S)) { velocity.y += speed; }
+        if (Keyboard::isKeyPressed(Keyboard::D)) { velocity.x += speed; }
 
         normalize(velocity, speed);
         entity.move(velocity);
-        cout << "Velocity " + to_string(velocity.x) + " | " + to_string(velocity.y) + "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
+        cout << "Velocity " + to_string(velocity.x) + " | " + to_string(velocity.y) + "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
 
         #pragma region Draw
             window.clear();
@@ -132,33 +93,3 @@ int main()
     }
     return 0;
 }
-
-/*
-        if (event.type == sf::Event::KeyPressed)
-        {
-            if (event.key.code == sf::Keyboard::Q) { activeKeys[LEFT] = true; }
-            if (event.key.code == sf::Keyboard::Z) { activeKeys[UP] = true; }
-            if (event.key.code == sf::Keyboard::S) { activeKeys[DOWN] = true; }
-            if (event.key.code == sf::Keyboard::D) { activeKeys[RIGHT] = true; }
-        }
-
-        if (event.type == sf::Event::KeyReleased)
-        {
-            if (event.key.code == sf::Keyboard::Q) { activeKeys[LEFT] = false; }
-            if (event.key.code == sf::Keyboard::Z) { activeKeys[UP] = false; }
-            if (event.key.code == sf::Keyboard::S) { activeKeys[DOWN] = false; }
-            if (event.key.code == sf::Keyboard::D) { activeKeys[RIGHT] = false; }
-        }
-
-        if (activeKeys[UP]) { sprite.move(0, -vit); }
-        if (!activeKeys[UP]) { sprite.move(0, 0); }
-
-        if (activeKeys[LEFT]) { sprite.move(-vit, 0); }
-        if (!activeKeys[LEFT]) { sprite.move(0, 0); }
-
-        if (activeKeys[RIGHT]) { sprite.move(vit, 0); }
-        if (!activeKeys[RIGHT]) { sprite.move(0, 0); }
-
-        if (activeKeys[DOWN]) { sprite.move(0, vit); }
-        if (!activeKeys[DOWN]) { sprite.move(0, 0); }
-*/
